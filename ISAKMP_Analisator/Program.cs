03f957aa7ddd0c34675fa73fc46431d2ee36ffd4
Cactus <Cactus@DESKTOP-TCL57OP>
2024-12-26 21:22:18 +0300

    Add project files.

---
+++ ISAKMP_Analisator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace program
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Создание конфигурации | Не знаю насколько это правильно, но нашел только это решение
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory) // Установка базового пути
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) // Добавление файла конфигурации
+                .Build();
+
+            // Считываем настройки
+            string inputDirectory = configuration["inputDirectory"];
+            Console.WriteLine($"Input Directory: {inputDirectory}");
+            var outputDirectory = configuration["outputDirectory"];
+            Console.WriteLine($"Output Directory: {outputDirectory}");
+
+
+
+        }
+    }
+}
