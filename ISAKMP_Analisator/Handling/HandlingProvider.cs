eba31c638e4fb12fbbc7ef519800cac8f135691e
EDV <edv-work@yandex.ru>
2025-01-24 00:16:56 +0300

    Example with small pcap

---
+++ ISAKMP_Analisator/Handling/HandlingProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace ISAKMP_Analisator.Handling
+{
+    public class HandlingProvider
+    {
+        private PcapReader pcapReader;
+
+        public HandlingProvider()
+        {
+            pcapReader = new PcapReader();
+        }
+
+        public void ReadPcapInput(string? inputDirectory)
+        {
+            if (!Directory.Exists(inputDirectory))
+            {
+                Console.WriteLine("ОШИБКА: не удалось получить доступ к входной директории!");
+
+                return;
+            }
+
+            // Пример: извлекаем пакеты из каждого pcap на входе
+            foreach (var file in Directory.EnumerateFiles(inputDirectory, "*", SearchOption.AllDirectories))
+            {
+                var packets = pcapReader.ReadPcap(file);
+
+                Console.WriteLine($"Из {file} извлечено {packets.Count()} ethernet пакетов");
+            }
+        }
+    }
+}
