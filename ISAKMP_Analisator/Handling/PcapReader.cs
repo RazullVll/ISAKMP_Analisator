a9fdde065326b4b079f965eeffaffb4997a96a3d
DmitryEf <edv-work@yandex.ru>
2025-01-26 22:27:11 +0300

    Example with small pcap (#1)

---
+++ ISAKMP_Analisator/Handling/PcapReader.cs
@@ -0,0 +1,67 @@
+﻿namespace ISAKMP_Analisator.Handling
+{
+    using PacketDotNet;
+    using SharpPcap;
+    using SharpPcap.LibPcap;
+
+    // Для примера
+    public class PcapReader
+    {
+        private List<EthernetPacket> packets = new();
+
+        private static int packetIndex = 0;
+
+        public IEnumerable<EthernetPacket> ReadPcap(string pcapFilePath)
+        {
+            ICaptureDevice device;
+
+            try
+            {
+                // Get an offline device
+                device = new CaptureFileReaderDevice(pcapFilePath);
+
+                // Open the device
+                device.Open();
+
+                // Подписываемся на событие о новом найденном пакете и по нему выполняем метод device_OnPacketArrival
+                device.OnPacketArrival +=
+                    new PacketArrivalEventHandler(device_OnPacketArrival);
+
+                // Запускает чтение дампа pcap. Здесь начинают срабатывать события OnPacketArrival
+                device.Capture();
+
+                device.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ОШИБКА: {e} при открытии файла {pcapFilePath}");
+            }
+
+            return this.packets;
+        }
+
+        private void device_OnPacketArrival(object sender, PacketCapture e)
+        {
+            var rawPacket = e.GetPacket();
+
+            var packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+
+            var ethernetPacket = packet.Extract<EthernetPacket>();
+
+            if (ethernetPacket != null)
+            {
+                this.packets.Add(ethernetPacket);
+
+                //Пример инфы, которую можно дёргать из пакетов
+                //Console.WriteLine("{0} At: {1}:{2}: MAC:{3} -> MAC:{4}",
+                //    packetIndex,
+                //    e.Header.Timeval.Date.ToString(),
+                //    e.Header.Timeval.Date.Millisecond,
+                //    ethernetPacket.SourceHardwareAddress,
+                //    ethernetPacket.DestinationHardwareAddress);
+
+                packetIndex++;
+            }
+        }
+    }
+}
--- ISAKMP_Analisator/Program.cs
+++ ISAKMP_Analisator/Program.cs
@@ -1,27 +1,71 @@
-﻿using System;
-using Microsoft.Extensions.Configuration;
+﻿// НЕОБХОДИМО УСТАНОВИТЬ WinPcap_4_1_3.exe из 3rdParty
+// pcap на вход в папке PcapExamples
 
-namespace Analisator
-
+//Название проекта принято формировать от названия решения (Solution - *.sln) и
+//в целом придерживаться нотации CamelCase в названиях.
+//Поэтому при создании правильней было бы назвать решение IsakmpAnalisator
+//а проект - IsakmpAnalisator.Processing
+//тогда namespace был бы IsakmpAnalisator
+namespace ISAKMP_Analisator
 {
+    using ISAKMP_Analisator.Handling;
+    using ISAKMP_Analisator.Settings;
+
     public class Program
     {
+        private static readonly SettingsProvider SettingsProvider = new();
+
+        private static readonly HandlingProvider HandlingProvider = new();
+
         public static void Main(string[] args)
         {
             // Создание конфигурации | Не знаю насколько это правильно, но нашел только это решение
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory) // Установка базового пути
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) // Добавление файла конфигурации
-                .Build();
+            // Всё верно, однако лучше выносить инициализацию в отдельные методы и классы
+            // Например:
+            if (!SettingsProvider.Init())
+            {
+                Console.ReadKey();
 
-            // Считываем настройки
-            string inputDirectory = configuration["inputDirectory"];
-            Console.WriteLine($"Input Directory: {inputDirectory}");
-            var outputDirectory = configuration["outputDirectory"];
-            Console.WriteLine($"Output Directory: {outputDirectory}");
+                return;
+            }
 
+            //Вообще в методе Main консольного приложения должно быть минимум кода,
+            //а основная логика раскидана по другим методам и классам. Рекомендую почитать это:
+            //https://habr.com/ru/articles/764898/
+            //https://habr.com/ru/companies/ruvds/articles/426413/
+            //И, собственно работа с SarpPcap
+            //https://habr.com/ru/sandbox/30450/
 
+            // Считываем настройки 
+            SettingsProvider.ShowSettingsInConsole();
 
+            while (true)
+            {
+                ShowActionsList();
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                    {
+                        // Пример считывания входной директории
+                        HandlingProvider.ReadPcapInput(SettingsProvider.GetInputPath());
+
+                        break;
+                    }
+                    case "2":
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void ShowActionsList()
+        {
+            Console.WriteLine("\r\nВыберите цифру действия:\r\n\t" +
+                              "1 - Обработать pcap-файлы\r\n\t" +
+                              "2 - Выход\r\n" +
+                              "и нажмите Enter\r\n");
         }
     }
 }
